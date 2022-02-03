c2ffe981f01344cd949e619528dac1d24f4fda45
DiegoEDG <1.5.dg.diego@gmail.com>
2022-02-03 12:44:57 -0600

    Funcionalidad de componente Presupuesto terminada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { PresupuestoForm } from './PresupuestoForm';
 
-export const Header = () => {
+export const Header = ({ presupuesto, setPresupuesto }) => {
 	return (
 		<header>
 			<h1>Planificador de Gastos</h1>
-			<PresupuestoForm />
+			<PresupuestoForm presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
 		</header>
 	);
 };
--- src/components/PresupuestoForm.jsx
+++ src/components/PresupuestoForm.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Mensaje } from './Mensaje';
 
-export const PresupuestoForm = () => {
+export const PresupuestoForm = ({ presupuesto, setPresupuesto }) => {
+	const [mensaje, setMensaje] = useState('');
+
+	const handlePresupuesto = (e) => {
+		e.preventDefault();
+
+		if (!presupuesto || presupuesto < 0) {
+			setMensaje('No es un presupuesto válido');
+			return;
+		}
+		setMensaje('');
+	};
+
 	return (
 		<div className="contenedor-presupuesto contenedor sombra">
-			<form className="formulario">
+			<form className="formulario" onSubmit={handlePresupuesto}>
 				<div className="campo">
 					<label>Define tu presupuesto</label>
-					<input className="nuevo-presupuesto" type="text" placeholder="$$$" />
+					<input
+						className="nuevo-presupuesto"
+						type="number"
+						placeholder="$$$"
+						onChange={(e) => setPresupuesto(Number(e.target.value))}
+					/>
 				</div>
 				<input type="submit" value="Agregar" />
+				{mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 			</form>
 		</div>
 	);
